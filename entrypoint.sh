9f46a55162552982efde49a33d1b5237274dd68f
Andre Fernandes <andre.fernandes86@gmail.com>
2025-01-22 22:21:43 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Generate SSH host keys if they do not exist
+if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+    ssh-keygen -A
+fi
+
 # Start the SSHD server for SFTP
 /usr/sbin/sshd -D &
 
